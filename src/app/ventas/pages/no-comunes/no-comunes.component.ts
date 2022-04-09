fc6cd1c0ede289745bd24b9bc7af890e9b55df16
VHMI <vhmizq@gmail.com>
2022-04-08 21:09:20 -0500

    finised proper pipes from angular

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs'; 
+import { tap} from 'rxjs/operators'
 
 @Component({
   selector: 'app-no-comunes',
@@ -35,5 +37,36 @@ export class NoComunesComponent  {
   quitarCliente() {
     return this.clientes.pop();
   }
+
+  //keyvalue PIpe
+  persona = {
+    nombre: 'Gotou',
+    edad: 19,
+    direccion: 'Tokyo, Japon'
+  }
+
+  heroe = [{
+    nombre: 'Robin',
+    vuela: 'false'
+  },
+  {
+    nombre: 'Raven',
+    vuela: 'true'
+  },
+  {
+    nombre: 'Chico bestia',
+    vuela: true
+  },
+  ]
+
+  //Async Pipe
+  miObservable = interval( 2000 )
+    .pipe( tap( () => console.log('intervalo')) ) // 0, 1,2, 3, 4
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+      resolve('Tenemos data de promesa')
+    }, 3500)
+  })
 
 }
