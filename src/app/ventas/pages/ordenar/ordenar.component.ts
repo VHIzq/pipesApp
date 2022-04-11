3ee424d8e69c1e7acba2ba675f67aacf9bf8d4dc
Victor Hugo <vrmq@gft.com>
2022-04-11 11:53:37 -0500

    pipeApp finished

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Heroe, Color } from '../../interfaces/ventas.interfaces';
+import { VuelaPipe } from '../../pipes/vuela.pipe';
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
@@ -9,8 +10,40 @@ import { Component } from '@angular/core';
 export class OrdenarComponent  {
 
     enMayusculas: boolean = true;
-    
+
+    ordenarPor: string = '';
+
+    heroes: Heroe[] = [
+      {
+        nombre: 'Robin',
+        vuela: false,
+        color: Color.rojo
+      },
+      {
+        nombre: 'Raven',
+        vuela: true,
+        color: Color.negro
+      },
+      {
+        nombre: 'Beast Boy',
+        vuela: true,
+        color: Color.verde
+      },
+      {
+        nombre: 'Star Fire',
+        vuela: true,
+        color: Color.azul
+      },
+
+    ]
+
     cambiarValor() {
       this.enMayusculas = !this.enMayusculas
     }
+
+    cambiarOrden( valor: string) {
+      this.ordenarPor = valor;
+      console.log(valor);
+    }
+
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,23 +1,31 @@
+// propios de angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// de terceros
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-
+//personalizados
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 
 @NgModule({
   declarations: [
+    //components
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
     OrdenarComponent,
+
+    //pipes
     MayusculasPipe,
+    VuelaPipe,
+    OrdenarPipe,
   ],
   exports: [
     NumerosComponent,
